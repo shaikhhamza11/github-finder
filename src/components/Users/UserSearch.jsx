b17ea7367f6f089a1692e44b360c3785bc7538fc
Hamza Shaikh <shaikh.hamza1199@gmail.com>
2022-04-06 01:38:00 +0530

    refactor : bug fix

--- src/components/Users/UserSearch.jsx
+++ src/components/Users/UserSearch.jsx
@@ -1,45 +1,46 @@
-import { useState } from 'react';
-import { useGithubContext } from '../../context/github/GithubContext';
-import { Actions } from './../../constants/Actions';
-import { useAlertContext } from '../../context/alert/AlertContext';
-import { searchUsers } from '../../context/github/GithubActions';
+import { useState } from "react";
+import { useGithubContext } from "../../context/github/GithubContext";
+import { Actions } from "./../../constants/Actions";
+import { useAlertContext } from "../../context/alert/AlertContext";
+import { searchUsers } from "../../context/github/GithubActions";
 const UserSearch = () => {
-  const [text, setText] = useState('');
+  const [text, setText] = useState("");
   const { users, dispatch } = useGithubContext();
   const { setAlert } = useAlertContext();
   const handleSubmit = async (e) => {
     e.preventDefault();
-    dispatch({ type: 'set-loading' });
-    if (text === '') {
-      setAlert('Please enter something', 'error', 3000);
+    dispatch({ type: "set-loading" });
+    if (text === "") {
+      setAlert("Please enter something", "error", 3000);
+      dispatch({ type: Actions.REMOVE_LOADING });
     } else {
       const users = await searchUsers(text);
 
       if (users.length === 0) {
-        setAlert('No matches found', 'error', 3000);
+        setAlert("No matches found", "error", 3000);
         dispatch({ type: Actions.REMOVE_LOADING });
       } else {
-        dispatch({ type: 'get-users', payload: { users } });
+        dispatch({ type: "get-users", payload: { users } });
       }
-      setText('');
+      setText("");
     }
   };
 
   return (
-    <div className='grid grid-cols-1 xl:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 mb-8 gap-8'>
+    <div className="grid grid-cols-1 xl:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 mb-8 gap-8">
       <div>
         <form onSubmit={handleSubmit}>
-          <div className='form-control'>
-            <div className='relative'>
+          <div className="form-control">
+            <div className="relative">
               <input
-                type='text'
-                className='w-full pr-20 bg-gray-200 input input-lg text-black'
+                type="text"
+                className="w-full pr-20 bg-gray-200 input input-lg text-black"
                 onChange={(e) => setText(e.target.value)}
                 value={text}
               />
               <button
-                type='submit'
-                className=' absolute top-0 right-0 rounded-l-none w-36 btn btn-lg'
+                type="submit"
+                className=" absolute top-0 right-0 rounded-l-none w-36 btn btn-lg"
               >
                 Go
               </button>
@@ -50,10 +51,10 @@ const UserSearch = () => {
       {users.length > 0 && (
         <div>
           <button
-            className='btn btn-ghost btn-lg'
+            className="btn btn-ghost btn-lg"
             onClick={() => {
               dispatch({ type: Actions.CLEAR_USERS });
-              setText('');
+              setText("");
             }}
           >
             Clear
